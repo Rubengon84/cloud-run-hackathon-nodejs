f06915f47b8952498c7b9130c59a127a594b61f2
Rubengon84 <rubengon.uk@gmail.com>
2023-06-26 20:06:19 +0100

    implementation Aquila code

--- web.js
+++ web.js
@@ -8,13 +8,156 @@ app.get('/', function (req, res) {
   res.send('Let the battle begin!');
 });
 
-app.post('/', function (req, res) {
-  console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
-  
+app.post('/', function (req, res) {  
   // TODO add your implementation here to replace the random response
   
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  res.send(requestProcess(req.body));
 });
 
 app.listen(process.env.PORT || 8080);
+
+
+const requestProcess = (bodyRequest) => {
+  /*********************************************/
+  //get Xmatch and Ymatch targets
+  const user = bodyRequest.arena.state["https://A_PLAYERS_URL"];
+  delete bodyRequest.arena.state["https://A_PLAYERS_URL"];
+  let arenaArray = Object.values(bodyRequest.arena.state);
+  let { Xmatch, Ymatch, nonMatching } = arenaArray.reduce(
+    (acc, userRecord) => {
+      userRecord.x === user.x && Math.abs(userRecord.y - user.y) <= 3
+        ? // or, if you need to compare by _id
+          // userRecord._id === user_id
+          acc.Xmatch.push(userRecord)
+        : userRecord.y === user.y && Math.abs(userRecord.y - user.y) <= 3
+        ? acc.Ymatch.push(userRecord)
+        : acc.nonMatching.push(userRecord);
+      return acc;
+    },
+    { Xmatch: [], Ymatch: [], nonMatching: [] }
+  );
+
+  Xmatch.sort((a, b) => {
+    return Math.abs(a.y - user.y) - Math.abs(b.y - user.y);
+  });
+
+  Ymatch.sort((a, b) => {
+    return Math.abs(a.x - user.x) - Math.abs(b.x - user.x);
+  });
+
+  //**************************************************************/
+
+  // const dodgeOrAttackX = (ourCharacter, enemy) => {
+  //   if (enemy.y - ourCharacter.y > 0) {
+  //     if (enemy.direction === "N" && ourCharacter.direction !== "S") {
+  //       if (["E", "W"].includes(ourCharacter.direction)) return "F";
+  //       else return "R";
+  //       // } else if (enemy.direction !== "N" && ourCharacter.direction === "S")
+  //       //   return "T";
+  //     } else if (enemy.direction !== "N" && ourCharacter.direction !== "S")
+  //       return "R";
+  //   } else if (enemy.y - ourCharacter.y < 0) {
+  //     if (enemy.direction === "S" && ourCharacter.direction !== "N") {
+  //       if (["E", "W"].includes(ourCharacter.direction)) return "F";
+  //       else return "L";
+  //       // } else if (enemy.direction !== "S" && ourCharacter.direction === "N")
+  //       //   return "T";
+  //     } else if (enemy.direction !== "S" && ourCharacter.direction !== "N")
+  //       return "L";
+  //   }
+  //   return "T";
+  // };
+
+  // const dodgeOrAttackY = (ourCharacter, enemy) => {
+  //   if (enemy.x - ourCharacter.x > 0) {
+  //     if (enemy.direction === "W" && ourCharacter.direction !== "E") {
+  //       if (["N", "S"].includes(ourCharacter.direction)) return "F";
+  //       else return "R";
+  //       // } else if (enemy.direction !== "W" && ourCharacter.direction === "E")
+  //       //   return "T";
+  //     } else if (enemy.direction !== "W" && ourCharacter.direction !== "E")
+  //       return "R";
+  //   } else if (enemy.y - ourCharacter.y < 0) {
+  //     if (enemy.direction === "E" && ourCharacter.direction !== "W") {
+  //       if (["N", "S"].includes(ourCharacter.direction)) return "F";
+  //       else return "L";
+  //       // } else if (enemy.direction !== "E" && ourCharacter.direction === "W")
+  //       //   return "T";
+  //     } else if (enemy.direction !== "E" && ourCharacter.direction !== "W")
+  //       return "L";
+  //   }
+  //   return "T";
+  // };
+
+  /***************************************************/
+  // Funtion to return the posible movements Array when the player in in the edge of the Arena
+  //new Set(["W", "S"]).has(player.y)
+  const moveCharacterOptions = (arenaX, arenaY, player) => {
+    if (
+      (player.x === arenaX && player.y === 0 && player.direction !== "S") ||
+      (player.x === arenaX &&
+        player.y === arenaY &&
+        player.direction !== "W") ||
+      (player.y === arenaY && player.x === 0 && player.direction !== "N") ||
+      (player.y === 0 && player.x === 0 && player.direction !== "E")
+    ) {
+      return "R";
+    } else if (
+      (player.y === 0 &&
+        ![0, arenaX].includes(player.x) &&
+        player.direction !== "E") ||
+      (player.x === 0 &&
+        ![0, arenaY].includes(player.y) &&
+        player.direction !== "N") ||
+      (player.x === arenaX &&
+        ![0, arenaY].includes(player.y) &&
+        player.direction !== "S") ||
+      (player.y === arenaY &&
+        ![0, arenaX].includes(player.x) &&
+        player.direction !== "W")
+    ) {
+      return "R";
+    } else {
+      return "F";
+    }
+  };
+  /******************************************************************************/
+  //Start the action of the player
+  //Shoot to other users
+  const [arenaX, arenaY] = bodyRequest.arena.dims;
+  if (Xmatch.length > 0) {
+    const onXTarget = user.y - Xmatch[0].y;
+    if (
+      (user.direction === "E" && onXTarget < 0) ||
+      (user.direction === "W" && onXTarget > 0)
+    ) {
+      return "T";
+    } else {
+      return moveCharacterOptions(arenaX, arenaY, user);
+    }
+  } else if (Ymatch.length > 0) {
+    const onYTarget = user.x - Ymatch[0].x;
+
+    if (
+      (user.direction === "N" && onYTarget > 0) ||
+      (user.direction === "S" && onYTarget < 0)
+    ) {
+      return "T";
+    } else {
+      return moveCharacterOptions(arenaX, arenaY, user);
+    }
+
+    /***************************************************************************/
+    // If player does not shoot, move player in the Arena
+  } else {
+    return moveCharacterOptions(arenaX, arenaY, user);
+  }
+};
+
+let bodyRequest = {
+  arena: arena,
+};
+let result = requestProcess(bodyRequest);
+console.timeEnd("dbsave");
+
+console.log(result);
