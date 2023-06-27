a9bf4a5b4303c2d30fa5980eec0fc6a3cde4e63a
Rubengon84 <rubengon.uk@gmail.com>
2023-06-27 08:26:38 +0100

    fix error 3

--- web.js
+++ web.js
@@ -29,11 +29,11 @@ const requestProcess = (bodyRequest) => {
   let arenaArray = Object.values(bodyRequest.arena.state);
   let { Xmatch, Ymatch, nonMatching } = arenaArray.reduce(
     (acc, userRecord) => {
-      userRecord.x === user.x && Math.abs(userRecord.y - user.y) <= 3
+      userRecord.x == user.x && Math.abs(userRecord.y - user.y) <= 3
         ? // or, if you need to compare by _id
           // userRecord._id === user_id
           acc.Xmatch.push(userRecord)
-        : userRecord.y === user.y && Math.abs(userRecord.y - user.y) <= 3
+        : userRecord.y == user.y && Math.abs(userRecord.y - user.y) <= 3
         ? acc.Ymatch.push(userRecord)
         : acc.nonMatching.push(userRecord);
       return acc;
@@ -54,26 +54,24 @@ const requestProcess = (bodyRequest) => {
   //new Set(["W", "S"]).has(player.y)
   const moveCharacterOptions = (arenaX, arenaY, player) => {
     if (
-      (player.x === arenaX && player.y === 0 && player.direction !== "S") ||
-      (player.x === arenaX &&
-        player.y === arenaY &&
-        player.direction !== "W") ||
-      (player.y === arenaY && player.x === 0 && player.direction !== "N") ||
-      (player.y === 0 && player.x === 0 && player.direction !== "E")
+      (player.x == arenaX && player.y == 0 && player.direction !== "S") ||
+      (player.x == arenaX && player.y == arenaY && player.direction !== "W") ||
+      (player.y == arenaY && player.x == 0 && player.direction !== "N") ||
+      (player.y == 0 && player.x == 0 && player.direction !== "E")
     ) {
       return "R";
     } else if (
-      (player.y === 0 &&
-        ![0, arenaX].includes(player.x) &&
+      (player.y == 0 &&
+        ![0, "0", arenaX].includes(player.x) &&
         player.direction !== "E") ||
-      (player.x === 0 &&
-        ![0, arenaY].includes(player.y) &&
+      (player.x == 0 &&
+        ![0, "0", arenaY].includes(player.y) &&
         player.direction !== "N") ||
-      (player.x === arenaX &&
-        ![0, arenaY].includes(player.y) &&
+      (player.x == arenaX &&
+        ![0, "0", arenaY].includes(player.y) &&
         player.direction !== "S") ||
-      (player.y === arenaY &&
-        ![0, arenaX].includes(player.x) &&
+      (player.y == arenaY &&
+        ![0, "0", arenaX].includes(player.x) &&
         player.direction !== "W")
     ) {
       return "R";
@@ -84,14 +82,13 @@ const requestProcess = (bodyRequest) => {
   /******************************************************************************/
   //Start the action of the player
   //Shoot to other users
-  const arenaX = bodyRequest.arena.dims[0];
-  const arenaY = bodyRequest.arena.dims[1];
-
+  const arenaX = bodyRequest.arena.dims[1];
+  const arenaY = bodyRequest.arena.dims[3];
   if (Xmatch.length > 0) {
-    const onXTarget = user.y - Xmatch[0].y;
+    const onXTarget = parseInt(user.y) - parseInt(Xmatch[0].y);
     if (
-      (user.direction === "E" && onXTarget < 0) ||
-      (user.direction === "W" && onXTarget > 0)
+      (user.direction == "E" && onXTarget < 0) ||
+      (user.direction == "W" && onXTarget > 0)
     ) {
       return "T";
     } else if (["N", "S"].includes(user.direction)) {
@@ -100,11 +97,11 @@ const requestProcess = (bodyRequest) => {
       return moveCharacterOptions(arenaX, arenaY, user);
     }
   } else if (Ymatch.length > 0) {
-    const onYTarget = user.x - Ymatch[0].x;
+    const onYTarget = parseInt(user.x) - parseInt(Ymatch[0].x);
 
     if (
-      (user.direction === "N" && onYTarget > 0) ||
-      (user.direction === "S" && onYTarget < 0)
+      (user.direction == "N" && onYTarget > 0) ||
+      (user.direction == "S" && onYTarget < 0)
     ) {
       return "T";
     } else if (["W", "E"].includes(user.direction)) {
